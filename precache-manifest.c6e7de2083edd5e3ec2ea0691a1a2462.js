0fbd1e3308258492330bc73d4eb5383f081a8d36
Taylor Radtke <taylorradtke@gmail.com>
2024-12-06 14:08:26 -0600

    Updates

---
+++ precache-manifest.c6e7de2083edd5e3ec2ea0691a1a2462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7176ff2f4559d925272a4ba8b38ae6de",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7ba19777302144899cc4",
+    "url": "/pokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f199046e92c4e224482",
+    "url": "/pokedex/static/js/2.94d94d9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokedex/static/js/2.94d94d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba19777302144899cc4",
+    "url": "/pokedex/static/js/main.bdbe956f.chunk.js"
+  },
+  {
+    "revision": "58c8577bd039be1b1b68",
+    "url": "/pokedex/static/js/runtime-main.2cd79c9c.js"
+  }
+]);
